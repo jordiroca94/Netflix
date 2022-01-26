4d783b13763845c4945ca185abb4e4e8af8a4071
Jordi Roca <jordirocasoler94@gmail.com>
2022-01-26 23:03:14 +0100

    page routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//using react-router-dom version 5.2.0 ( does not work with version 6)
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from './constants/routes';
 
 export default function App() {
-  return(
-  <>
-  <JumbotronContainer />
-  <FaqsContainer/>
-  <FooterContainer/>
-  </>
-  )}
+  return (
+    <Router>
+      <Route exact path="/browse">
+        <Browse />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
+  );
+}
