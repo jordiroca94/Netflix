7e5cc47097f2326fb27cbc3bc4b94f7fe382e868
Jordi Roca <jordirocasoler94@gmail.com>
2022-01-31 13:24:24 +0100

    signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
 
 export default function Signin() {
-  return <p>Hello from the signIN </p>;
+  const [emailAdress, setEmailAdress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAdress === "";
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Signin</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email adress"
+              value={emailAdress}
+              onChange={({ target }) => setEmailAdress(target.value)}
+            />
+            <Form.Input
+              type="Password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In{" "}
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New To Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            {" "}
+            This page is protected by Google reCAPTCHA to ensure you are not a
+            bot. Learn more.{" "}
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      ;
+      <FooterContainer />
+    </>
+  );
 }
